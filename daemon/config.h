5fd757353233e39e61cc428a731b8669c1b035f1
Dave997 <davide.ambrosi-1@studenti.unitn.it>
2018-05-09 23:07:18 +0200

    function write_log added

--- daemon/config.h
+++ daemon/config.h
@@ -3,6 +3,7 @@
 #define EXTID  (12)
 
 #define TMPLEN (100)
+#define MAXLEN 200
 
 #include <stdio.h>
 #include <sys/ipc.h>
@@ -10,8 +11,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
+typedef char* string;
 char *tmpfiletpl="queue.tmp";
+const string DEF_TXT = "log.txt";
+const string DEF_CSV = "log.csv";
 
 int msgkey(int mode) { // mode == 0 for READ, 1 for write
 	char tmpfile[TMPLEN];
@@ -37,3 +42,72 @@ int msgkey(int mode) { // mode == 0 for READ, 1 for write
 	//printf("extref='%s', tmpfile='%s' (ft=%d), extid='%d', msgkey='%d'\n", EXTREF, tmpfile, ft, EXTID, key);
 	return (key);
 }
+
+/*
+if message type = 1 --> txt
+if message type = 2 --> csv
+if name file is empty keep the default log file
+*/
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[MAXLEN];
+	char file_name[MAXLEN];
+};// message
+
+void write_log(struct mesg_buffer *msg)
+{
+	FILE* loggher_logs = fopen("/tmp/logger_logs.txt", "a");
+	FILE* f;
+
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	fprintf(loggher_logs, "%d:%d:%d) message received \"%s\"\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text);
+	
+	if(strlen(msg->mesg_text) > 0) 
+	{
+		if(strstr(msg->mesg_text, "quit") != NULL || strstr(msg->mesg_text, "kill") != NULL) 
+		{
+			fprintf(loggher_logs, "%d:%d:%d) logger terminated\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text);
+			exit(0);
+		} 
+		else 
+		{
+			if(!strlen(msg->file_name) > 0)
+			{
+				switch(msg->mesg_type)
+				{
+					case 1:
+						f = fopen(DEF_TXT, "a");
+						fprintf(loggher_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text, msg->file_name);
+						break;
+					case 2:
+						f = fopen(DEF_CSV, "a");
+						fprintf(loggher_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text, msg->file_name);
+						break;
+					default:
+						fprintf(loggher_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text);
+						break;
+				}//end switch
+			}
+			else
+			{
+				//TO DO: extract file path from file_name
+				switch(msg->mesg_type)
+				{
+					case 1:
+						f = fopen(DEF_TXT, "a");
+						fprintf(loggher_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text, msg->file_name);
+						break;
+					case 2:
+						f = fopen(DEF_CSV, "a");
+						fprintf(loggher_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text, msg->file_name);
+						break;
+					default:
+						fprintf(loggher_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec, msg->mesg_text);
+						break;
+				}//end switch
+			}//end if(!strlen(msg->file_name) > 0)
+		}//end if(strstr...!=NULL)
+	}//end if(strlen(message.mesg_text) > 0) 
+
+}//end write_log
--- daemon/logger.c
+++ daemon/logger.c
@@ -12,29 +12,27 @@
 
 
 #define MAXLEN 200
- 
+ /*
 // structure for message queue
 struct mesg_buffer {
     long mesg_type;
     char mesg_text[MAXLEN];
 	char file_name[MAXLEN];
-} message;
+} message;*/
 
-const char *DEF_TXT = "log.txt";
-const char *DEF_CSV = "log.csv";
+struct mesg_buffer message;
 
-int main() {
-	
-	//primo fork
+int main() 
+{	
 	int fid = fork();
+
 	if(fid < 0){
-		printf("?ERROR\n");
+		printf("?ERROR while forking\n");
 		exit(1);
 	}
 	
 	if(fid == 0){
 		//figlio
-		daemon(1, 1);
 		key_t key = msgkey(0);
 		int msgid = msgget(key, 0666);
 		
@@ -46,61 +44,22 @@ int main() {
 		} else {
 			exit(0);//If exist, exit
 		}
+
+		daemon(1, 1);
 		
 		//int end = 1;
 		while(1) {
 			msgrcv(msgid, &message, sizeof(message), 1, 0);
 			
-			if(strlen(message.mesg_text) > 0) {
-				if(strstr(message.mesg_text, "quit") != NULL || strstr(message.mesg_text, "kill") != NULL) {
-					//end = 0;
-					msgctl(msgid, IPC_RMID, NULL);
-					exit(0);
-				} else {
-					//if message type = 1 --> txt
-					//if message type = 2 --> csv
-					//if name file is empty keep the default log file
-					FILE *f;
-					if(!strlen(message.file_name) > 0){
-						switch(message.mesg_type){
-							case 1:
-								f = fopen(DEF_TXT, "a");
-								break;
-							case 2:
-								f = fopen(DEF_CSV, "a");
-								break;
-							default:
-								//Non dovrebbe succedere
-								f = fopen(DEF_TXT, "a");
-								break;
-						}
-					} else {
-						f = fopen(message.file_name, "a");
-					}
-					
-
-					
-					if (f == NULL){
-						//TODO: mandare segnale d'errore
-						printf("Error opening file!\n");
-						//end = 0;
-						msgctl(msgid, IPC_RMID, NULL);
-						exit(0);
-					} else {
-						fprintf(f, "%s", message.mesg_text);
-					}
-
-					fclose(f);
-				}
-			}
-			//Di dubbia utilita
+			write_log(&message);
+			
+			//clear message buffer
 			message.mesg_text[0] = '\0';
 		}
 		
 		//Elimina la message queue
 		msgctl(msgid, IPC_RMID, NULL);
 	}
-	
 	
 	
 	else {
