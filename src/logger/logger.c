3b63957a2f2b0f4c579fb7ec7a4b8878c01b2a58
Dave997 <davide.ambrosi-1@studenti.unitn.it>
2018-05-11 11:28:02 +0200

    write_log finished

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -20,8 +20,18 @@
  * 
  * */
 
+FILE* logger_logs;
+time_t t;
+struct tm tm;
+
 int main()
 {
+	logger_logs = fopen("/tmp/logger_logs.txt", "a");
+	t = time(NULL);
+	tm = *localtime(&t);
+
+	fprintf(logger_logs, "%d:%d:%d) logger started\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+
 	int fid = fork(); //Genero figlio
 	msg message;
 	if (fid < 0)
@@ -32,40 +42,43 @@ int main()
 
 	if (fid == 0)
 	{
+		fprintf(logger_logs, "%d:%d:%d) figlio generato\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 		//Figlio
-		int fid2 = fork(); //Genero nipote
-
-		if (fid2 < 0)
+		int fid2 = fork();
+		
+		if(fid < 0)
 		{
-			printf("?ERROR nel fork del figlio\n");
+			fprintf(logger_logs, "%d:%d:%d) Daemon fork FAILED!\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 			exit(1);
 		}
 
-		if (fid2 == 0)
+		if(fid == 0)	//Nipote (demone)
 		{
-			//Nipote (demone)
-			msg message;
-			daemon(1, 1); //unistd
+				fprintf(logger_logs, "%d:%d:%d) nipote generato\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-			key_t key = msgkey(0);
-			int msgid = msgget(key, 0666 | IPC_CREAT);
+				//msg message;
+				daemon(1, 1); //unistd.h
 
-			while (1)
-			{
-				msgrcv(msgid, &message, sizeof(message), 1, 0);
-				write_log(&message);
-				//clear message buffer
-				message.mesg_text[0] = '\0';
-			}
+				//Creo la message queue
+				key_t key = msgkey(0);
+				int msgid = msgget(key, 0666 | IPC_CREAT);
 
-			//Elimina la message queue
-			msgctl(msgid, IPC_RMID, NULL);
+				while (strcmp(message.mesg_text, "quit") != 0)
+				{
+					msgrcv(msgid, &message, sizeof(message), 1, 0);
+					write_log(&message);
+					//clear message buffer
+					message.mesg_text[0] = '\0';
+				}
+
+				//Elimina la message queue
+				msgctl(msgid, IPC_RMID, NULL);
 		}
-		else
-		{
-			//Figlio
-			exit(0); //L'unica funzione del figlio e' creare il nipote
-		}
+		else //Figlio
+		{ 
+			fprintf(logger_logs, "%d:%d:%d) figlio muore\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+			exit(0); //L'unica funzione del figlio e' creare il nipote e morire
+		}//end if
 	}
 	else
 	{
@@ -90,20 +103,23 @@ int main()
 
 		while (1)
 		{
-
 			printf("> ");
 			r = (long)fgets(msgtxt, MAXLEN, stdin);
 
+			// terminate logger
 			if (strstr(msgtxt, "exit") != NULL)
 			{
+				fprintf(logger_logs, "%d:%d:%d) logger terminated\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 				exit(0);
 			}
 
 			strcpy(message.mesg_text, msgtxt);
 			msgsnd(msgid, &message, sizeof(message), 0);
 
+			// terminate logger and kill daemon
 			if (strstr(msgtxt, "quit") != NULL)
 			{
+				fprintf(logger_logs, "%d:%d:%d) logger terminated and daemon killed\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 				exit(0);
 			}
 		}
@@ -112,62 +128,64 @@ int main()
 	return 0;
 }
 
+
 void write_log(msg *m)
 {
-	FILE *logger_logs = fopen("/tmp/logger_logs.txt", "a");
-	FILE *f;
+	FILE* f;
 
-	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
 	fprintf(logger_logs, "%d:%d:%d) message received \"%s\"\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text);
-
-	if (strlen(m->mesg_text) > 0)
+	
+	if(strlen(m->mesg_text) > 0) 
 	{
-		if (strstr(m->mesg_text, "quit") != NULL || strstr(m->mesg_text, "kill") != NULL)
+		if(strstr(m->mesg_text, "quit") != NULL || strstr(m->mesg_text, "kill") != NULL) 
 		{
-			fprintf(logger_logs, "%d:%d:%d) logger terminated\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+			fprintf(logger_logs, "%d:%d:%d) logger terminated\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text);
 			exit(0);
-		}
-		else
+		} 
+		else 
 		{
-			if (!strlen(m->file_name) > 0)
+			if(!strlen(m->file_name) > 0)
 			{
-				switch (m->mesg_type)
+				/*
+					if message type = 1 --> txt
+					if message type = 2 --> csv
+					if name file is empty keep the default log file
+				*/
+				switch(m->mesg_type)
 				{
-				case 1:
-					f = fopen(DEF_TXT, "a");
-					fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->file_name);
-					fprintf(f, "%s\n", m->mesg_text);
-					break;
-				case 2:
-					f = fopen(DEF_CSV, "a");
-					fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->file_name);
-					fprintf(f, "%s", m->mesg_text);
-					break;
-				default:
-					fprintf(logger_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
-					break;
-				} //end switch
+					case 1:
+						f = fopen(DEF_TXT, "a");
+						fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text, m->file_name);
+						fprintf(f, "%s\n", m->mesg_text);
+						break;
+					case 2:
+						f = fopen(DEF_CSV, "a");
+						fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text, m->file_name);
+						fprintf(f, "%s", m->mesg_text);
+						break;
+					default:
+						fprintf(logger_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text);
+						break;
+				}//end switch
 			}
 			else
 			{
-				//TO DO: extract file path from file_name
-				switch (m->mesg_type)
+				switch(m->mesg_type)
 				{
-				case 1:
-					f = fopen(DEF_TXT, "a");
-					fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->file_name);
-					break;
-				case 2:
-					f = fopen(DEF_CSV, "a");
-					fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->file_name);
-					break;
-				default:
-					fprintf(logger_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
-					break;
-				} //end switch
-			}		//end if(!strlen(m->file_name) > 0)
-		}			//end if(strstr...!=NULL)
-	}				//end if(strlen(message.mesg_text) > 0)
+					case 1:
+						f = fopen(m->file_name, "a");
+						fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text, m->file_name);
+						break;
+					case 2:
+						f = fopen(m->file_name, "a");
+						fprintf(logger_logs, "%d:%d:%d) message recorded in %s\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text, m->file_name);
+						break;
+					default:
+						fprintf(logger_logs, "%d:%d:%d) ERR: mesg_type not recognized!\n", tm.tm_hour, tm.tm_min, tm.tm_sec, m->mesg_text);
+						break;
+				}//end switch
+			}//end if(!strlen(msg->file_name) > 0)
+		}//end if(strstr...!=NULL)
+	}//end if(strlen(message.mesg_text) > 0) 
 
-} //end write_log
+}//end write_log
