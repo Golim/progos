d73765dcbe50f586df7c4b0c5cd3988c0da3f540
osboxes.org <matteogolinelli97@gmail.com>
2018-05-17 13:00:31 -0400

    Udates to stats.c

--- src/stats/stats.c
+++ src/stats/stats.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	msg *m;
 	char cmd[COMMAND_MAX_LEN];
-	char sep[3];
+	char sep[SEP_MAX_LEN];
 
 	// Working var
 	int i;
@@ -28,6 +28,7 @@ int main(int argc, char **argv)
 	strcpy(sep, ",");
 
 	bool mu = TRUE;
+	bool names = FALSE;
 	
 	if (argc < 2)
 	{
@@ -87,10 +88,28 @@ int main(int argc, char **argv)
 					valido = TRUE;
 				}
 			}
+			else if (strcmp(option, "--separator") == 0 || strcmp(option, "-s") == 0)
+			{
+				if (strcmp(value, "") != 0)
+				{
+					strcpy(sep, value);
+					valido = TRUE;
+				}
+			}
+		}
+		else if (strcmp(argv[i], "--names") == 0 || strcmp(argv[i], "-n") == 0)
+		{
+			names = TRUE;
+			valido = TRUE;
 		}
 		else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
 		{
 			print_help();
+			exit(0);
+		}
+		else if (strcmp(argv[i], "--usage") == 0 || strcmp(argv[i], "-u") == 0)
+		{
+			print_usage();
 			exit(0);
 		}
 			
@@ -106,7 +125,7 @@ int main(int argc, char **argv)
 
 	char *str = malloc(sizeof(char) * STATS_MAX_LEN);
 
-	stats(cmd, str, sep, mu);
+	stats(cmd, str, sep, mu, names);
 	strcpy(m->msg_log.txt, str);
 	send_to_logger(m);
 	free(m);
@@ -117,7 +136,7 @@ void send_to_logger(msg *m)
 	printf("\n ------------------------------------ \n | %li | %s | %s | -->\n ------------------------------------\n", m->type, m->msg_log.txt, m->msg_log.fn);
 }
 
-int stats(char *cmd, char *stat, char *sep, bool mu) //mu: stampa unità di misura se true
+int stats(char *cmd, char *stat, char *sep, bool mu, bool names)
 {
 	int codice_ritorno;
 	long durata_realtime; //in ms
@@ -147,11 +166,17 @@ int stats(char *cmd, char *stat, char *sep, bool mu) //mu: stampa unità di misu
 	//Durata CPU-time
 	durata_cputime = (clock_finish_cputime.tv_sec - clock_start_cputime.tv_sec) * 1000000 + (clock_finish_cputime.tv_nsec - clock_start_cputime.tv_nsec) / 1000;
 
-	sprintf(stat, "%s", nome_comando);
+	if(names == TRUE)
+		sprintf(stat, "%s[cmd]", stat);
+	sprintf(stat, "%s%s", stat, nome_comando);
 	strcat(stat, sep);
+	if(names == TRUE)
+		sprintf(stat, "%s[args]", stat);
 	strcat(stat, argomenti);
 	strcat(stat, sep);
 	
+	if(names == TRUE)
+		sprintf(stat, "%s[duration_realtime]", stat);
 	if(mu == TRUE)
 		sprintf(stat, "%s%lims", stat, durata_realtime);
 	else
@@ -159,12 +184,15 @@ int stats(char *cmd, char *stat, char *sep, bool mu) //mu: stampa unità di misu
 
 	strcat(stat, sep);
 	
+	if(names == TRUE)
+		sprintf(stat, "%s[duration_CPU]", stat);
 	if(mu == TRUE)
 		sprintf(stat, "%s%lius", stat, durata_cputime);
 	else
 		sprintf(stat, "%s%li", stat, durata_cputime);
 
-	
+	if(names == TRUE)
+		sprintf(stat, "%s[return_code]", stat);
 	strcat(stat, sep);
 	sprintf(stat, "%s%d", stat, codice_ritorno);
 
@@ -207,11 +235,13 @@ void print_usage()
 
 void print_help()
 {
-	printf("Usage:  stats [OPTION]... [COMMAND]\n");
-	printf("Execute COMMAND , log various statistics in a file\n");
-	printf("The command MUST be the last argument passed to stats");
-	printf("Example:  stats  ls \n");//Aggiungere che se ci sono anche argomenti il comando va passato tra virgolette
-	printf("\n");
+	printf("\
+Usage:  stats [OPTION]... [COMMAND]\n\
+Execute COMMAND , log various statistics in a file\n\
+The command MUST be the last argument passed to stats\n\
+Example:  stats  ls \n\
+If the command has argument, the command has to be in quotes\n\
+Example:  stats \"ls -al\"\n\n");
 
 	printf("-OPTION\n");
 	printf("All OPTION are not mandatory. Contemplated options are:\n");
@@ -219,7 +249,8 @@ void print_help()
 	//-f --format
 	printf("%10s","-f=[v], ");
 	printf("%-20s","--format=[v]");
-	printf("%s","specifies the format for the output. [v] can be 'csv' or 'txt'\n"); //Aggiungere default="txt"(??)
+	printf("%s","specifies the format for the output.\n\
+                               [v] can be 'csv' or 'txt'\n"); //Aggiungere default="txt"(??)
 	
 	//-lf --logfile
 	printf("%10s","-lf=[v], ");
@@ -229,12 +260,31 @@ void print_help()
 	//-mu --measure-unit
 	printf("%10s","-mu=[v], ");
 	printf("%-20s","-measure-units=[v]");
-	printf("%s","specifies whether the output should contain the unit of measurement. [v] can be 'true' or 'false'\n");
+	printf("%s","specifies whether the output should contain the unit of measurement.\n\
+                               [v] can be 'true' or 'false'\n");
+    
+	//-n --names
+	printf("%10s","-n, ");
+	printf("%-20s","--names");
+	printf("%s","specifies whether the output should contain the names of the fields.\n");
 	
 	//-h --help
 	printf("%10s","-h, ");
 	printf("%-20s","--help");
 	printf("%s","display this help and exit\n");
+	
+	//-s --separator
+	printf("%10s","-s=[v], ");
+	printf("%-20s","--separator=[v]");
+	printf("%s","specifies the separator that appears between the fields.\n\
+                               [v] can be a character or a string \n\
+                               in quotes if it is composed only of spaces \n\
+                               or if it contains special characters(|, &..) . e.g. -s=\" | \"\n"); //Definire meglio gli special characters
+	
+	//-u --usage
+	printf("%10s","-u, ");
+	printf("%-20s","--usage");
+	printf("%s","give a short usage message\n");
 	
 	printf("\n");
 }
--- src/stats/stats.h
+++ src/stats/stats.h
@@ -1,11 +1,12 @@
 // Const Value
 const long TIME_FACTOR = 1000000;
 const long COMMAND_MAX_LEN = 255;
-const long STATS_MAX_LEN = 255;
+const long STATS_MAX_LEN = 500;
+const long SEP_MAX_LEN = 20;
 const long ARG_MAX_LEN = 255;
 
 // Functions
-int stats(char *cmd, char *stat, char *sep, bool mu);
+int stats(char *cmd, char *stat, char *sep, bool mu, bool names);
 int separe_command_args(char *cmd, char *name, char *arg);
 void send_to_logger(msg *m);
 void print_usage();
